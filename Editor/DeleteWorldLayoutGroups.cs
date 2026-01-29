24bd593cd3d6051dc642033f2a529d7f3dce0fb4
Alihan <74368418+Alihan-4108@users.noreply.github.com>
2026-01-29 11:37:41 +0300

    Build-Time Removal from All Scenes

---
+++ Editor/DeleteWorldLayoutGroups.cs
@@ -0,0 +1,34 @@
+#if UNITY_EDITOR
+
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeleteWorldLayoutGroups : IProcessSceneWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnProcessScene(Scene scene, BuildReport report)
+    {
+        int removedCount = 0;
+
+        foreach (var root in scene.GetRootGameObjects())
+        {
+            var groups = root.GetComponentsInChildren<WorldLayoutGroup>(true);
+
+            foreach (var group  in groups)
+            {
+                Object.DestroyImmediate(group, true);
+                removedCount++;
+            }
+        }
+
+        if (removedCount > 0)
+        {
+            Debug.Log($"Scene '{scene.name}': Removed {removedCount} WorldLayoutGroup component(s).");
+        }
+    }
+}
+
+#endif
